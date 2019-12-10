40fd913d67e36a0b1e58c381802ffdc48feb3d98
Patricio Testolin <patricio.testolin@dift.co>
2019-12-10 19:59:44 -0300

    add product list

---
+++ src/components/product-box.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductBox = () => {
+  return (
+    <div className="product__grid__box">
+      <div className="product__grid__box__image"></div>
+      <div className="product__grid__box__data">
+        <h3 className="product__grid__box__data__title">Reloj de pie</h3>
+        <p className="product__grid__box__data__code">TF-06234</p>
+        <span className="product__grid__box__data__prize">$25</span>
+      </div>
+    </div>
+  )
+}
+
+export default ProductBox
--- src/views/product-detail.js
+++ src/views/product-detail.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import ProductBox from '../components/product-box'
 
 const ProductDetailView = () => {
   return (
@@ -8,102 +9,51 @@ const ProductDetailView = () => {
         <section className="product__herospace">
           <h2 className="product__herospace__title">Flores</h2>
         </section>
-        <section className="product__aside">
-          <h2 className="product__aside__title">Flores</h2>
-          <a href="#" className="product__aside__group">Ramos</a>
-          <a href="#" className="product__aside__link">Linea económica</a>
-          <a href="#" className="product__aside__link">Linea premium</a>
-          <a href="#" className="product__aside__group">Varas</a>
-          <a href="#" className="product__aside__link">Linea económica</a>
-          <a href="#" className="product__aside__link">Linea premium</a>
-          <h2 className="product__aside__title">Follaje</h2>
-          <a href="#" className="product__aside__group">Plantas</a>
-          <a href="#" className="product__aside__group">Plantas con maceta</a>
-          <a href="#" className="product__aside__group">Matas &amp; jardín vertical</a>
-          <a href="#" className="product__aside__group">Complementos</a>
-          <h2 className="product__aside__title">Frutas</h2>
-          <h2 className="product__aside__title">Navidad</h2>
-          <a href="#" className="product__aside__group">Pinos</a>
-          <a href="#" className="product__aside__group">Decoración</a>
-          <a href="#" className="product__aside__group">Flores &amp; follaje</a>
-          <a href="#" className="product__aside__group">Adornos para pino</a>
-          <h2 className="product__aside__title">Recipientes</h2>
-          <a href="#" className="product__aside__group">Floreros</a>
-          <a href="#" className="product__aside__group">Macetas</a>
-          <h2 className="product__aside__title">Sale</h2>
-        </section>
-      </section>
-      <section className="product__grid">
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
-          </div>
-        </div>
-        <div className="product__grid__box">
-          <div className="product__grid__box__image"></div>
-          <div className="product__grid__box__data">
-            <h3 className="product__grid__box__data__title">Reloj de pie</h3>
-            <p className="product__grid__box__data__code">TF-06234</p>
-            <span className="product__grid__box__data__prize">$25</span>
+        <div className="product__content">
+          <section className="product__aside">
+            <h2 className="product__aside__title">Flores</h2>
+            <a href="#" className="product__aside__group">Ramos</a>
+            <a href="#" className="product__aside__link">Linea económica</a>
+            <a href="#" className="product__aside__link">Linea premium</a>
+            <a href="#" className="product__aside__group">Varas</a>
+            <a href="#" className="product__aside__link">Linea económica</a>
+            <a href="#" className="product__aside__link">Linea premium</a>
+            <h2 className="product__aside__title">Follaje</h2>
+            <a href="#" className="product__aside__group">Plantas</a>
+            <a href="#" className="product__aside__group">Plantas con maceta</a>
+            <a href="#" className="product__aside__group">Matas &amp; jardín vertical</a>
+            <a href="#" className="product__aside__group">Complementos</a>
+            <h2 className="product__aside__title">Frutas</h2>
+            <h2 className="product__aside__title">Navidad</h2>
+            <a href="#" className="product__aside__group">Pinos</a>
+            <a href="#" className="product__aside__group">Decoración</a>
+            <a href="#" className="product__aside__group">Flores &amp; follaje</a>
+            <a href="#" className="product__aside__group">Adornos para pino</a>
+            <h2 className="product__aside__title">Recipientes</h2>
+            <a href="#" className="product__aside__group">Floreros</a>
+            <a href="#" className="product__aside__group">Macetas</a>
+            <h2 className="product__aside__title">Sale</h2>
+          </section>
+          <section className="product__grid">
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+            <ProductBox />
+          </section>
+          <div className="product__paginator">
+            <div className="product__paginator__content">
+              <a href="#" className="product__paginator__arrow"><img src="/images/paginator-l.svg" /></a>
+              <a href="#" className="product__paginator__number">1</a>
+              <a href="#" className="product__paginator__number">2</a>
+              <a href="#" className="product__paginator__number">3</a>
+              <a href="#" className="product__paginator__number">4</a>
+              <a href="#" className="product__paginator__arrow"><img src="/images/paginator-r.svg" /></a>
+            </div>
           </div>
         </div>
       </section>
